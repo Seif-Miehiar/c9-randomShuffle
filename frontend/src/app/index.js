7604d58e66d2373ecf5f3455823701d2c8a332d3
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-12-16 15:03:46 +0200

    init server

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { NavBar } from "../components";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// import { MoviesList, MoviesInsert, MoviesUpdate } from "../pages/index";
 function App() {
 	return (
 		<Router>
 			<NavBar />
+			<Switch>
+				{/* <Route path="/movies/list" exact component={MoviesList} />
+				<Route path="/movies/create" exact component={MoviesInsert} />
+				<Route path="/movies/update/:id" exact component={MoviesUpdate} /> */}
+			</Switch>
 		</Router>
 	);
 }
