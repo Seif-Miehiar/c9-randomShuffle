cb90dfd943f4cf82c1c1e21099746b4579b75e05
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-12-22 16:37:11 +0200

    init

--- frontend/src/components/landingPage.jsx
+++ frontend/src/components/landingPage.jsx
@@ -4,26 +4,49 @@ import axios from 'axios';
 //this component for rendering the shuffled student
 const LandingPage = () => {
 
-  const [data, setData] = useState({ users: [] });
+  const [students, setStudents] = useState({ users: [] });
+  // const [shuffledStudent, setShuffledStudent] = useState({ oneUser: "" })
+  const [random, setRandom] = useState({ randomInt: 0 })
+  const [randomUser, setRandomUser] = useState({ user: {} })
 
   useEffect(() => {
 
     axios.get(`http://localhost:3000/api/users`)
       .then(res => {
-        setData({ data: res.data })
+        setStudents({ students: res.data })
       })
       .catch((err) => {
         if (err) throw err
       })
 
   });
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  const onClickRandom = (event) => {
+    event.preventDefault();
+    const users = students.students.data
+    const randomIndex = getRandomInt(users.length)
+    setRandom({ randomInt: randomIndex })
+    const randomStudent = users[randomIndex]
+    setRandomUser({ user: users[randomIndex] })
+    // setShuffledStudent({ oneUser: randomStudent })
+    // console.log(shuffledStudent)
+    console.log(randomUser.user)
+  }
 
 
   return <div>
     Hello
-    <button onClick={() => console.log("test")}>
+    <button onClick={onClickRandom}>
       Shuffle
     </button>
+    <div>
+      Today's Technical Solution is on {randomUser.user.name}
+    </div>
+    {/* <div key={shuffledStudent._id}>
+      Today's Technical Solution is on {shuffledStudent}
+    </div> */}
   </div>
 }
 
